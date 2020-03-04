90ed1444da0f407820ec2e025f3a9c4a76f0ca4a
Laurentiu Tagarici <lurul@Laurentius-MacBook-Air.local>
2020-03-04 18:05:22 +0200

    project setup

---
+++ lab-1/src/ro/unitbv/javadatatypes/Exercise1.java
@@ -0,0 +1,12 @@
+package ro.unitbv.javadatatypes;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+
+	}
+
+}
