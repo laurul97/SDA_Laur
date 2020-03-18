b74745e03d8be9cdf43ad0b0f95dd48764de32ec
Laurentiu Tagarici <laurentiudre97@gmail.com>
2020-03-18 17:38:33 +0200

    added to github

--- lab-1/src/ro/unitbv/javadatatypes/Exercise1.java
+++ lab-1/src/ro/unitbv/javadatatypes/Exercise1.java
@@ -1,5 +1,7 @@
 package ro.unitbv.javadatatypes;
 
+import java.util.Scanner;
+
 public class Exercise1 {
 
 	public static void main(String[] args) {
@@ -7,6 +9,39 @@ public class Exercise1 {
 		
 		System.out.println("Hello World");
 
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Introdu nr1: ");
+		int nr1  = scanner.nextInt();
+		
+		//afisam output-ul
+		System.out.println("primul nr citit este: " + nr1);
+		
+		System.out.println("Introdu nr2: ");	
+		int nr2 = scanner.nextInt();
+		
+		System.out.println("al doilea nr citit este: " + nr2);
+		
+		int sum = nr1 + nr2;
+		System.out.println("suma este: " + sum);
+		
+		int dif = nr1 - nr2;
+		System.out.println("Diferenta: " + dif);
+		
+		int prod = nr1 * nr2;
+		System.out.println("Produsul: " + prod);
+		
+		double impartire = 0;
+		
+		
+		int media = (nr1 + nr2)/2;
+		System.out.println("Media:" + media);
+		
+		if (nr1 < nr2) {
+			System.out.println("max integer: " + nr2);
+		} else {
+			System.out.println("max integer: " + nr1);
+		}
+	
+		}
 	}
 
-}
